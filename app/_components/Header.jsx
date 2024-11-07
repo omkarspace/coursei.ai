77e68dc6a3a580f6e1abe46062ceedf037938fac
omkarspace <omkarspace@gmail.com>
2024-11-07 22:49:13 +0530

    changes in UI

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 function header() {
   return (
     <div className='flex justify-between p-5 shadow-md'>
         <Image src={'/logo.svg'} width={100} height={100} />
+        <Link href={'/dashboard'}>
         <Button>Get started</Button>
+        </Link>
     </div>
   )
 }
 
 export default header
+
+
+{/* <Link href={userCourseList>=5?'/dashboard/upgrade':'/create-course '}>
+            <Button>+ Create AI Course</Button>
+        </Link> */}
