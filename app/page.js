53efd4e72c1fd942772b7b8f3770ca5c3533071e
omkarspace <omkarspace@gmail.com>
2026-06-01 14:43:02 +0530

    feat: compose landing page with new section components

--- app/page.js
+++ app/page.js
@@ -1,15 +1,25 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Header from "./_components/Header";
+import Navbar from "./_components/Navbar";
 import Hero from "./_components/Hero";
-import FooterBlock from "./_components/Footer";
+import Features from "./_components/Features";
+import HowItWorks from "./_components/HowItWorks";
+import TechnicalArchitecture from "./_components/TechnicalArchitecture";
+import Community from "./_components/Community";
+import Pricing from "./_components/Pricing";
+import FAQ from "./_components/FAQ";
+import Footer from "./_components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      {/* <Header/> */}
+    <>
+      <Navbar />
       <Hero />
-      <FooterBlock />
-    </div>
+      <Features />
+      <HowItWorks />
+      <TechnicalArchitecture />
+      <Community />
+      <Pricing />
+      <FAQ />
+      <Footer />
+    </>
   );
 }
